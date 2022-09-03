36629229d49ac30af342a43014e5083b6bf28a0d
Jeff Free <jefffreelance3@gmail.com>
2022-09-03 00:38:11 -0300

    image upload finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,76 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import NewPost from "./components/NewPost";
 
 function App() {
+	const [file, setFile] = useState(null);
+	const [image, setImage] = useState(null);
+
+	useEffect(() => {
+		const getImage = () => {
+			const img = new Image();
+			img.src = URL.createObjectURL(file);
+
+			img.onload = () => {
+				setImage({
+					url: img.src,
+					width: 650,
+					height: 450,
+				});
+			};
+		};
+
+		file && getImage();
+	}, [file]);
+
 	return (
 		<div>
 			<Navbar />
-			<div className="newPostCard">
-				<div className="addPost">
-					<img
-						src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-						alt=""
-						className="avatar"
-					/>
-					<div className="postForm">
-						<input
-							type="text"
-							placeholder="What's on your mind"
-							className="postInput"
+			{image ? (
+				<NewPost image={image} setImage={setImage} />
+			) : (
+				<div className="newPostCard">
+					<div className="addPost">
+						<img
+							src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+							alt=""
+							className="avatar"
 						/>
-						<label htmlFor="file">
-							<img
-								className="addImg"
-								src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
-								alt=""
+						<div className="postForm">
+							<input
+								type="text"
+								placeholder="What's on your mind"
+								className="postInput"
 							/>
-							<img
-								className="addImg"
-								src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
-								alt=""
+							<label htmlFor="file">
+								<img
+									className="addImg"
+									src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Octicons-cloud-upload.svg/1200px-Octicons-cloud-upload.svg.png"
+									alt=""
+								/>
+								<img
+									className="addImg"
+									src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Octicons-cloud-upload.svg/1200px-Octicons-cloud-upload.svg.png"
+									alt=""
+								/>
+								<img
+									className="addImg"
+									src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Octicons-cloud-upload.svg/1200px-Octicons-cloud-upload.svg.png"
+									alt=""
+								/>
+								<button>Send</button>
+							</label>
+							<input
+								onChange={(e) => setFile(e.target.files[0])}
+								style={{ display: "none" }}
+								type="file"
+								id="file"
 							/>
-							<img
-								className="addImg"
-								src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
-								alt=""
-							/>
-							<button>Send</button>
-						</label>
-						<input
-							style={{ display: "none" }}
-							type="file"
-							id="file"
-						/>
+						</div>
 					</div>
 				</div>
-			</div>
-			<NewPost />
+			)}
 		</div>
 	);
 }
--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useEffect, useRef } from "react";
 import * as faceapi from "face-api.js";
 
-const NewPost = () => {
+const NewPost = ({ image, setImage }) => {
+	const { url, width, height } = image;
 	const imgRef = useRef();
 	const canvasRef = useRef();
 
@@ -20,13 +21,13 @@ const NewPost = () => {
 		);
 
 		faceapi.matchDimensions(canvasRef.current, {
-			width: 940,
-			height: 650,
+			width: 650,
+			height: 450,
 		});
 
 		const resized = faceapi.resizeResults(detections, {
-			width: 940,
-			height: 650,
+			width: 650,
+			height: 450,
 		});
 		faceapi.draw.drawDetections(canvasRef.current, resized);
 		faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
@@ -47,7 +48,31 @@ const NewPost = () => {
 		imgRef.current && loadModels();
 	}, []);
 
-	return <div>NewPost</div>;
+	return (
+		<div className="container">
+			<div className="left" style={{ width, height }}>
+				<img
+					className="mainImg"
+					ref={imgRef}
+					crossOrigin="anonymous"
+					src={url}
+					alt={url}
+				/>
+				<canvas ref={canvasRef}></canvas>
+			</div>
+			<div className="right">
+				<h1>Share your post</h1>
+				{/* <input
+					type="text"
+					className="rightInput"
+					placeholder="What's on your mind?"
+				/> */}
+				<button className="rightButton" onClick={() => setImage(null)}>
+					back
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default NewPost;
